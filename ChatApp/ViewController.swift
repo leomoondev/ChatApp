9878cc6c53d6e007c9c5e0c7a6395877ffa07bf3
Leo Moon <leomoondev@gmail.com>
2017-03-22 14:39:57 -0700

    Add a Login Page Using Constraint Anchors

--- ChatApp/ViewController.swift
+++ ChatApp/ViewController.swift
@@ -12,14 +12,14 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func handleLogout() {
+        let loginController = LoginController()
+        present(loginController, animated: true, completion: nil)
     }
-
-
 }
 
